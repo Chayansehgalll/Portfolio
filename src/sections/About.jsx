7d148eaa4bc32fac6d0aeb50e2b8ea60468a5301
Chayan Sehgal <chayansehgal2971@gmail.com>
2026-05-06 02:39:06 +0530

    Fix About.jsx imports for deployment

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,7 +1,7 @@
 import { useRef } from "react";
-import Card from "../components/Card";
+import { Card } from "../components/Card";
 import { Globe } from "../components/Globe";
-import CopyEmailButton from "../components/CopyEmailButton";
+import { CopyEmailButton } from "../components/CopyEmailButton";
 import { Frameworks } from "../components/Frameworks";
 
 const About = () => {
