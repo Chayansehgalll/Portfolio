4608ed38ff6eb293ee2f46bcee070719bdbba523
Chayan Sehgal <chayansehgal2971@gmail.com>
2026-05-06 03:02:49 +0530

    build: replace ParallaxBackground with TempBackground and fix Hero imports

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,6 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import HeroText from "../components/HeroText";
-import { ParallaxBackground } from "../components/ParallaxBackground";
+import { TempBackground } from "../components/TempBackground";
 import { Astronaut } from "../components/Astronaut";
 import { Float } from "@react-three/drei";
 import { useMediaQuery } from "react-responsive";
@@ -17,7 +17,7 @@ const Hero = () => {
       className="flex items-start justify-center min-h-screen overflow-hidden md:items-start md:justify-start c-space"
     >
       <HeroText />
-      <ParallaxBackground />
+      <TempBackground />
       <figure
         className="absolute inset-0"
         style={{ width: "100vw", height: "100vh" }}
